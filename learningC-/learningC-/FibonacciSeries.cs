fddddedcdb1fcf4bd93e748923efccd278a303d1
JellyFishhhhhh <jellyfishow@gmail.com>
2023-01-02 17:22:11 -0500

    update

---
+++ learningC-/learningC-/FibonacciSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace learningC_
+{
+	public class FibonacciSeries
+	{
+		public int getFib(int i)
+		{
+			if (i == 0)
+			{
+				return 0;
+			}
+			else if(i == 1)
+			{
+				return 1;
+			}
+			return getFib(i-1) + getFib(i-2);
+			
+		}
+
+	//	static void Main(string[] args)
+	//	{
+	//		FibonacciSeries f = new FibonacciSeries();
+	//		int number, result;
+	//		Console.Write("Enter i-th number of the element: ");
+	//		number = int.Parse(Console.ReadLine());
+	//		result = f.getFib(number-1);
+ //           Console.Write(result);
+ //       }
+	}
+}
+
---
+++ learningC-/learningC-/checkPrime.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//namespace learningC_
+//{
+//	public class checkPrime
+//	{
+//		static void Main(string[] args)
+//			{
+//			int number,p,isPrime = 1;
+//            Console.Write("Enter the number to check: ");
+//            number = int.Parse(Console.ReadLine());
+//			p = number / 2;
+//			for(int i = p; i>= 2; i--)
+//			{
+//				if(number%i == 0)
+//				{
+//					isPrime = 0;
+//                    Console.Write("Not a prime");
+//					break;
+//                }
+//			}
+//			if (isPrime == 1)
+//			{
+//				Console.Write("Is a prime");
+//			}
+//        }
+//	}
+//}
+
